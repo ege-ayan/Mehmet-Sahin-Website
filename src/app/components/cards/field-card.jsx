a16e7007ccc21722397fb9d5aef7d22f491a2e4f
Ege Ayan <egeayan2478@gmail.com>
2024-10-26 14:51:32 +0300

    fields page completed

--- src/app/components/cards/field-card.jsx
+++ src/app/components/cards/field-card.jsx
@@ -4,17 +4,18 @@ import Link from "next/link";
 export default function FieldCard({ title, imageUrl, href }) {
   return (
     <Link href={href}>
-      <div className="relative block w-80 h-48 rounded-lg overflow-hidden shadow-lg group">
+      <div className="relative block w-full h-full rounded-lg overflow-hidden shadow-xl group">
         <Image
           src={imageUrl}
           alt={title}
-          layout="fill"
+          fill
           objectFit="cover"
-          className="transition-transform duration-300 group-hover:scale-105"
+          className="transition-transform duration-500 group-hover:scale-110"
           priority
+          quality={100}
         />
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
-        <h2 className="absolute bottom-4 left-4 text-white text-xl font-semibold z-10">
+        <h2 className="absolute bottom-8 left-8 text-white text-3xl font-semibold z-10">
           {title}
         </h2>
       </div>
--- src/app/sections/home-fields.jsx
+++ src/app/sections/home-fields.jsx
@@ -1,15 +1,47 @@
 import FieldCard from "../components/cards/field-card";
 
 export default function HomeFields() {
+  const fields = [
+    { title: "Retina", imageUrl: "/images/field-retina.jpg", href: "/retina" },
+    {
+      title: "Göz Estetiği",
+      imageUrl: "/images/field-eye-aesthatic.jpg",
+      href: "/cataract",
+    },
+    {
+      title: "Excimer Lazer",
+      imageUrl: "/images/field-excimer-laser.jpg",
+      href: "/glaucoma",
+    },
+    {
+      title: "Keratokonus",
+      imageUrl: "/images/field-keratokonus.jpg",
+      href: "/laser",
+    },
+  ];
+
   return (
-    <div className="flex flex-col gap-10 items-center py-16 justify-center">
-      <div className="text-gray-900 text-5xl font-bold">UZMANLIK ALANLARIM</div>
-      <div>
-        <FieldCard
-          title="Retina"
-          imageUrl="/images/field-retina.jpg"
-          href="/retina"
-        />
+    <div className="flex flex-col gap-5 items-center py-16 justify-center">
+      <div className="text-gray-900 text-3xl md:text-5xl font-bold text-center">
+        UZMANLIK ALANLARIM
+      </div>
+      <div className="container flex flex-wrap gap-10 items-center justify-center p-5">
+        {fields.map((field, index) => (
+          <div key={index} className="w-full md:max-w-[700px]">
+            <div
+              className="relative w-full"
+              style={{ paddingBottom: "56.25%" }}
+            >
+              <div className="absolute inset-0">
+                <FieldCard
+                  title={field.title}
+                  imageUrl={field.imageUrl}
+                  href={field.href}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
