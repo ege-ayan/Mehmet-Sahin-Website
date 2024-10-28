1ca340800fd7fdcf0dd3757e7bdd1c4d213063cf
Ege Ayan <egeayan2478@gmail.com>
2024-10-28 13:48:51 +0300

    glokom desc fixed

--- src/app/blogs/glokom/page.jsx
+++ src/app/blogs/glokom/page.jsx
@@ -5,14 +5,14 @@ export default function Glokom() {
     <BlogPage title="GLOKOM" imageSrc="/images/field-excimer-laser.jpg">
       <>
         <p>
-          Akıllı mercekler, yakın, orta ve uzak mesafelerde kesintisiz net görüş
-          sağlayan üç odaklı (trifokal) merceklerdir. Katarakt cerrahisi
-          sonrası, uygun gözlerde göz içine yerleştirilir. Akıllı mercek
-          uygulanan hastalarda ömür boyu refraksiyon sabit kalır ve her mesafede
-          net görüş devam eder. Son yıllarda trifokal merceklere ilave olarak,
-          Edof teknolojisi ile üretilen mercekler de hayatımıza girmiştir. Bu
-          merceklerde ara ve uzak mesafelerde net görüş sağlanırken, yakın
-          mesafede ince bir yakın gözlüğüne ihtiyaç duyabiliriz.
+          Halk arasında göz tansiyonu ve karasu hastalığı olarak da bilinen
+          glokom sık görülen bir göz hastalığıdir ve dünyada en önemli kalıcı
+          körlük nedenidir. Sinsi bir hastalıktır. Çoğu zaman hiçbir semptom
+          vermeden ilerler ve göz sinirinde kalıcı hasara neden olur. Bu nedenle
+          özellikle 40 yaş sonrası düzenli göz kontrolleri ihmal edilmemelidir.
+          Aile hikayesi, miyopi varlığı, diyabet, hipertansiyon, 40 yaş üstü
+          olmak ve göze travma hikayesi göz tansiyonu riskini arttıran
+          faktörlerdir.
         </p>
       </>
     </BlogPage>
--- src/app/blogs/smart-lens/page.jsx
+++ src/app/blogs/smart-lens/page.jsx
@@ -4,14 +4,14 @@ export default function SmartLens() {
   return (
     <BlogPage title="AKILLI MERCEK" imageSrc="/images/smart-lens.jpg">
       <p>
-        Akıllı mercek yakın, orta ve uzak mesafelerde kesintisiz net görüş
-        sağlayan üç odaklı (trifokal) merceklerdir. Katarakt cerrahisi sonrası
+        Akıllı mercekler, yakın, orta ve uzak mesafelerde kesintisiz net görüş
+        sağlayan üç odaklı (trifokal) merceklerdir. Katarakt cerrahisi sonrası,
         uygun gözlerde göz içine yerleştirilir. Akıllı mercek uygulanan
         hastalarda ömür boyu refraksiyon sabit kalır ve her mesafede net görüş
-        devam eder . Son yıllarda trifokal merceklere ilave olarak Edof
-        teknolojisi ile üretilen merceklerde hayatımıza girmiştir. Bu
-        merceklerde ara ve uzak mesafelerde net görüş sağlanırken yakın mesafede
-        ince bir yakın gözlüğüne ihtiyaç duyabiliriz.
+        devam eder. Son yıllarda trifokal merceklere ilave olarak, Edof
+        teknolojisi ile üretilen mercekler de hayatımıza girmiştir. Bu
+        merceklerde ara ve uzak mesafelerde net görüş sağlanırken, yakın
+        mesafede ince bir yakın gözlüğüne ihtiyaç duyabiliriz.
       </p>
     </BlogPage>
   );
