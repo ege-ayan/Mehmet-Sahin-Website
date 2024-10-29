fe222be3a791aa74ea71faf03f0b8bd515c4ad84
Ege Ayan <egeayan2478@gmail.com>
2024-10-29 14:16:55 +0300

    permanent redirect for about page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/tr/about.html",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
